3efe94df003faff6f14b8a6328c278bcce8a0c1d
Slity <0911kjw@yu.ac.kr>
2025-05-23 14:47:56 +0900

    navbar 오류 수정

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -5,10 +5,6 @@ import Navbar from './Navbar'
 const Layout = () => {
   return (
     <div>
-        
-            <div className='text-center align-middle text-5xl bg-blue-900 text-white shadow-lg pt-7 pb-7'> 
-                <strong>Univ List</strong>
-            </div>
         <Navbar/>
         <Outlet/>
     </div>
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
         <div className='text-center align-middle text-5xl bg-blue-900 text-white shadow-lg pt-7 pb-7'> 
             <strong>Univ List</strong>
         </div>
-        <div className='flex justify-between items-center bg-navy-200 shadow-lg p-4'>
+        <div className='flex justify-between items-center bg-navy-200 shadow-lg px-30 py-2'>
             <div className="hover:bg-blue-800  hover:text-white hover:rounded-lg p-1 font-bold">Asia</div>
             <div className="hover:bg-blue-800  hover:text-white hover:rounded-lg p-1 font-bold">Europe</div>
             <div className="hover:bg-blue-800  hover:text-white hover:rounded-lg p-1 font-bold">North America</div>
